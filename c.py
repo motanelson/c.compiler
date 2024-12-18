db6f10e8f365df56b01b6a6c69fb599fb26c9875
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2024-12-18 18:55:09 +0000

    Add files via upload

--- c.py
+++ c.py
@@ -6,10 +6,16 @@ files=input("give me a .c file? ")
 f1=open(files,"r")
 codes=f1.read()
 f1.close()
-source_file = io.StringIO(codes)
-obj = cc(source_file, 'x86_64')
-obj=link([obj])
-# Salve o objeto
-f1=open(files+".o","wb")
-f1.write(obj.sections[1].data)
-f1.close()
+try:
+    source_file = io.StringIO(codes)
+    obj = cc(source_file, 'x86_64')
+    obj=link([obj])
+except Exception as e:
+    print(e)
+    print ("error:")
+     
+else:
+    # Salve o objeto
+    f1=open(files+".o","wb")
+    f1.write(obj.sections[1].data)
+    f1.close()
